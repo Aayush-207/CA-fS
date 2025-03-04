f1ef32136069ea94c0b8b7df669b4c529c7410b5
Aayush-207 <aayushraut2006@gmail.com>
2025-03-04 14:24:46 +0530

    -

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -3,14 +3,23 @@ import React from "react";
 function TestimonialCard() {
 
     const prop = {
-        name: "Emily Watson",
-        review: "This service was amazing! Highly recommended"
+        name1: "Emily Watson",
+        review1: "This service was amazing! Highly recommended",
+        name2: "John Smith",
+        review2: "Nice for this service"
     }
     return(
+        <>
         <div className="testimonial-card">
-            <h3>Name: {prop.name}</h3>
-            <p>Description: {prop.review}</p>
+            <h3>Name: {prop.name1}</h3>
+            <p>Description: {prop.review2}</p>
         </div>
+        <div className="testimonial-card">
+            <h3>Name: {prop.name2}</h3>
+            <p>Description: {prop.review2}</p>
+        </div>
+        </>
+        
     )
 }
 
